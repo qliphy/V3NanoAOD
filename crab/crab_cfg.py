ff246a363f17af8bc9c65d094e8213d72034273a
Qiang <qiang.li@cern.ch>
2019-09-02 08:51:39 +0200

    crab cfg

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -4,7 +4,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'Nano_ww_2016_v2'
+config.General.requestName = 'Nano_v2'
 config.General.transferLogs=True
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
@@ -15,17 +15,19 @@ config.JobType.sendPythonFolder	 = True
 config.JobType.allowUndistributedCMSSW = True
 
 config.section_("Data")
-config.Data.inputDataset = '/WW_TuneCUETP8M1_13TeV-pythia8/RunIISummer16NanoAODv4-PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6_ext1-v1/NANOAODSIM'
-#config.Data.inputDBS = 'phys03'
-config.Data.inputDBS = 'global'
+#config.Data.inputDataset = '/WW_TuneCUETP8M1_13TeV-pythia8/RunIISummer16NanoAODv4-PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6_ext1-v1/NANOAODSIM'
+#config.Data.inputDBS = 'global'
+config.Data.inputDataset = '/VVV-3000-R008/qili-crab_VVV_NanoAOD_v3-f9255f5ef092ed4261a677d1b3d4c5e7/USER'
+config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 #config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
-
 #config.Data.outLFNDirBase = '/store/user/%s/NanoPost' % (getUsernameFromSiteDB())
 config.Data.publication = False
 config.Data.outputDatasetTag = 'NanoTest_v2'
+#config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
+
 config.section_("Site")
 config.Site.storageSite = "T2_CH_CERN"
 
